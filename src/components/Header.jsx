0046410ac17ae34bdce5f23d375760afd4e3b2f9
GeorgiiVoyakin <voyaking@gmail.com>
2022-02-28 12:50:59 +0300

    Some header

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+// импорт файла со стилями
+import '../styles/Header.css'
+
+function Header() {
+    return (
+    <header className='header'>
+        <img src='https://www.idearebel.com/wp-content/uploads/2020/10/React-Idea-Rebel.png' alt='' />
+    </header>
+    );
+}
+
+export default Header
